0e6b025cae577d4bcfc8f32134a178941da17785
Kasun Thennakoon <tmkasun@gmail.com>
2014-10-14 17:11:50 +0530

    Siddhi Extension - Geo event notifier

---
+++ siddhi-extensions/geo/geo-event-notifier/src/test/java/org/wso2/cep/geo/notify/GeoNotifyTestCase.java
@@ -0,0 +1,51 @@
+package org.wso2.cep.geo.notify;
+
+import org.apache.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.wso2.siddhi.core.SiddhiManager;
+import org.wso2.siddhi.core.config.SiddhiConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class GeoNotifyTestCase {
+
+    private static Logger logger = Logger.getLogger(NotifyAlert.class);
+
+    protected static SiddhiManager siddhiManager;
+
+    protected long start;
+    protected long end;
+
+    public abstract void setUpChild();
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        logger.info("Init Siddhi");
+
+        SiddhiConfiguration siddhiConfiguration = new SiddhiConfiguration();
+
+        List<Class> extensions = new ArrayList<Class>(6);
+        extensions.add(NotifyAlert.class);
+
+        siddhiConfiguration.setSiddhiExtensions(extensions);
+
+        siddhiManager = new SiddhiManager(siddhiConfiguration);
+
+    }
+
+    @Before
+    public void setUpChildren() {
+        setUpChild();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        Thread.sleep(1000);
+        logger.info("Shutting down Siddhi");
+        siddhiManager.shutdown();
+    }
+
+}
