114b00b88f6265f95770a2a399a24f68e110ca3a
Kasun Thennakoon <tmkasun@gmail.com>
2014-10-14 17:10:15 +0530

    Siddhi Extension - Geo fencing

---
+++ siddhi-extensions/geo/geo-event-fuser/src/main/java/org/wso2/cep/geo/libs/ExecutionPlanSubscriber.java
@@ -0,0 +1,99 @@
+package org.wso2.cep.geo.libs;
+
+/*
+*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+import org.apache.log4j.Logger;
+import org.wso2.siddhi.core.config.SiddhiContext;
+import org.wso2.siddhi.core.event.Event;
+import org.wso2.siddhi.core.event.in.InEvent;
+import org.wso2.siddhi.core.event.in.InListEvent;
+import org.wso2.siddhi.core.event.in.InStream;
+import org.wso2.siddhi.core.executor.expression.ExpressionExecutor;
+import org.wso2.siddhi.core.query.processor.transform.TransformProcessor;
+import org.wso2.siddhi.query.api.definition.Attribute;
+import org.wso2.siddhi.query.api.definition.StreamDefinition;
+import org.wso2.siddhi.query.api.expression.Expression;
+import org.wso2.siddhi.query.api.expression.Variable;
+import org.wso2.siddhi.query.api.extension.annotation.SiddhiExtension;
+import org.wso2.siddhi.query.compiler.SiddhiCompiler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.wso2.siddhi.query.compiler.SiddhiCompiler.*;
+
+/*
+* TODO: 1. Where is "outStreamDefinition" (spatialEventsInnerStream) reside in the CEP?
+*       3. Ability to get current active `geo_` pattern execution plans count for pre initialize the static var in `observer` class
+* */
+
+@SiddhiExtension(namespace = "geo", function = "subscribeExecutionPlan")
+public class ExecutionPlanSubscriber extends TransformProcessor {
+    private static Logger logger = Logger.getLogger(ExecutionPlanSubscriber.class);
+    private Map<String, Integer> paramPositions = new HashMap<String, Integer>();
+    Boolean initialized = false;
+    public ExecutionPlanSubscriber() {
+//        this.outStreamDefinition = parseStreamDefinition("define stream dataIn (id string, latitude double, longitude double, eventId string, state string, information string )");
+//        upCount();
+
+    }
+    @Override
+    protected InStream processEvent(InEvent inEvent) {
+        return inEvent;
+    }
+    @Override
+    protected InStream processEvent(InListEvent inListEvent) {
+        return inListEvent;
+    }
+    @Override
+    protected Object[] currentState() {
+        return new Object[]{paramPositions};
+    }
+    @Override
+    protected void restoreState(Object[] objects) {
+        if (objects.length > 0 && objects[0] instanceof Map) {
+            paramPositions = (Map<String, Integer>) objects[0];
+        }
+    }
+    @Override
+    protected void init(Expression[] parameters, List<ExpressionExecutor> expressionExecutors, StreamDefinition inStreamDefinition, StreamDefinition outStreamDefinition, String elementId, SiddhiContext siddhiContext) {
+        logger.info("Calling init");
+        this.outStreamDefinition = this.inStreamDefinition;
+        if (!initialized){
+            upCount();
+            this.initialized = true;
+        }
+    }
+    @Override
+    public void destroy() {
+        downCount();
+    }
+
+    void upCount(){
+        ExecutionPlansCount.upCount();
+        logger.info("DEBUG: upCountNumberOfExecutionPlans current count after update = " + ExecutionPlansCount.getNumberOfExecutionPlans());
+    }
+
+    void downCount(){
+        ExecutionPlansCount.downCount();
+        logger.info("DEBUG: downCountNumberOfExecutionPlans current count after update = " + ExecutionPlansCount.getNumberOfExecutionPlans());
+    }
+
+}
---
+++ siddhi-extensions/geo/geo-event-fuser/src/test/java/org/wso2/cep/geo/libs/FuseEventsTestCase.java
@@ -0,0 +1,104 @@
+package org.wso2.cep.geo.libs;
+
+import org.apache.log4j.Logger;
+import org.junit.*;
+import org.wso2.siddhi.core.SiddhiManager;
+import org.wso2.siddhi.core.config.SiddhiConfiguration;
+import org.wso2.siddhi.core.event.Event;
+import org.wso2.siddhi.core.query.output.callback.QueryCallback;
+import org.wso2.siddhi.core.stream.input.InputHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class FuseEventsTestCase {
+    private static Logger logger = Logger.getLogger(FuseEventsTestCase.class);
+    private static List<String[]> data;
+
+    protected static SiddhiManager siddhiManager;
+
+    protected long start;
+    protected long end;
+
+
+    @Test
+    public void testProcess() throws Exception {
+        logger.info("TestProcess");
+        String eventSubscribeExecutionPlan = "from dataIn#transform.geo:subscribeExecutionPlan() \n" +
+                "select id, latitude, longitude, 1412236 as timeStamp, 12.6 as speed, 123.12 as heading, eventId, state, information\n" +
+                "insert into dataOut;";
+
+        start = System.currentTimeMillis();
+        // Add multiple execution plans which are using the same input stream(dataIn) and outputting to same output stream(dataOut) ,allowing create same copy of the event coming from dataIn stream
+        String queryReference1 = siddhiManager.addQuery(eventSubscribeExecutionPlan);
+
+        String queryReference2 = siddhiManager.addQuery(eventSubscribeExecutionPlan);
+
+        String queryReference3 = siddhiManager.addQuery(eventSubscribeExecutionPlan);
+
+
+        String eventFuseExecutionPlan = "from dataOut#window.geo:eventsFunion(eventId) \n" +
+                "select id, latitude, longitude, timeStamp, speed, heading, state , information, 'Testing' as notify\n" +
+                "insert into dataFusedOut;";
+        String eventFuseQueryReference = siddhiManager.addQuery(eventFuseExecutionPlan);
+
+        end = System.currentTimeMillis();
+        logger.info(String.format("Time to add query: [%f sec]", ((end - start) / 1000f)));
+        siddhiManager.addCallback(eventFuseQueryReference, new QueryCallback() {
+            @Override
+            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                Integer eventsCount = 0;
+                for (Event event : inEvents) {
+                    eventsCount++;
+                }
+                Assert.assertEquals("1",eventsCount.toString());
+            }
+        });
+        generateEvents();
+    }
+
+
+    private void generateEvents() throws Exception {
+        InputHandler inputHandler = siddhiManager.getInputHandler("dataIn");
+        for (String[] dataLine : data) {
+            inputHandler.send(new Object[]{dataLine[0], dataLine[1], dataLine[2], dataLine[3], dataLine[4], dataLine[5]});
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        logger.info("Init Siddhi setUp");
+
+        SiddhiConfiguration siddhiConfiguration = new SiddhiConfiguration();
+
+        List<Class> extensions = new ArrayList<Class>(6);
+        extensions.add(FuseEvents.class);
+        extensions.add(ExecutionPlanSubscriber.class);
+
+        siddhiConfiguration.setSiddhiExtensions(extensions);
+
+        siddhiManager = new SiddhiManager(siddhiConfiguration);
+
+
+        logger.info("DEBUG: calling setUpChild");
+        siddhiManager.defineStream("define stream dataIn (id string, latitude double, longitude double, eventId string, state string, information string )");
+
+        data = new ArrayList<String[]>();
+
+        data.add(new String[]{"km-4354", "12.56", "56.32", UUID.randomUUID().toString(), "NORMAL", "NORMAL driving pattern"});
+        //TODO: uncomment below lines for more data feed
+//        data.add(new String[]{"km-4354", "12.56", "56.32", UUID.randomUUID().toString(), "NORMAL", "NOT NORMAL driving pattern"});
+//        data.add(new String[]{"km-4354", "12.56", "56.32", UUID.randomUUID().toString(), "NORMAL", "NOT NORMAL driving pattern"});
+//        data.add(new String[]{"km-4354", "12.56", "56.32", UUID.randomUUID().toString(), "NORMAL", "NORMAL driving pattern"});
+
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        Thread.sleep(1000);
+        logger.info("Shutting down Siddhi");
+        siddhiManager.shutdown();
+    }
+}
